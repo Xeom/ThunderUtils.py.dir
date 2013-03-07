f801aa558893e5e8be8c9e65db43d27aff2b526f
Xeom <fwharf.fw@googlemail.com>
2013-03-07 21:32:40 +0000

    Sprucing up the place
    Made stuff neater, added a few args, fixed a few things.

--- Aliases.py
+++ Aliases.py
@@ -12,35 +12,40 @@ itemnamewhitelist = "1234567890abcdeflmnok"
 # Time Commands
 @hook.command("day", description="Set your time to day.")
 def onCommandDay(sender,args):
+    
     sender.sendMessage("Your time was set to day.")
     sender.setPlayerTime(6000,0)
+    
     return True
 
 @hook.command("night", descriptoin="Set your time to night.")
 def onCommandNight(sender,args):
+    
     sender.sendMessage("Your time was set to night.")
     sender.setPlayerTime(18000,0)
+    
     return True
 
 # Fix lag
 @hook.command("fixlag", description="Clears out minecarts,arrows,items, etc.")
 def onCommandFixLag(sender, args):
+    
     bukkit.Bukkit.dispatchCommand(sender, "rem items -1")
     bukkit.Bukkit.dispatchCommand(sender, "rem arrows -1")
     bukkit.Bukkit.dispatchCommand(sender, "rem boats -1")
-    bukkit.Bukkit.dispatchCommand(sender, "rem paintings -1")
     bukkit.Bukkit.dispatchCommand(sender, "rem xp -1")
 
     bukkit.Bukkit.dispatchCommand(sender, "butcher -f")
 
     sudo("save-all")
 
-    sender.sendMessage("You fixed teh lags!")
+    sender.sendMessage("T3h lagz, they be gone!")
     return True
     
 # Effects
 @hook.command("e", description="List potion effect NBT values.")
 def onCommandE(sender,args):
+    
     if len(args) != 1:
         sender.sendMessage("01 - "+color("b")+"Speed")
         sender.sendMessage("02 - "+color("9")+"Slowness")
@@ -49,6 +54,7 @@ def onCommandE(sender,args):
         sender.sendMessage("05 - "+color("c")+"Strength")
         sender.sendMessage(color("6")+"Page "+color("c")+"1 "+color("6")+"of "+color("c")+"4")
         return True
+    
     if args[0] == "2":
         sender.sendMessage("06 - "+color("4")+"Health")
         sender.sendMessage("07 - "+color("8")+"Damage")
@@ -57,6 +63,7 @@ def onCommandE(sender,args):
         sender.sendMessage("10 - "+color("d")+"Regeneration")
         sender.sendMessage(color("6")+"Page "+color("c")+"2 "+color("6")+"of "+color("c")+"4")
         return True
+    
     if args[0] == "3":
         sender.sendMessage("11 - "+color("5")+"Resistance")
         sender.sendMessage("12 - "+color("6")+"Fire Resistance")
@@ -65,6 +72,7 @@ def onCommandE(sender,args):
         sender.sendMessage("15 - "+color("8")+"Blindness")
         sender.sendMessage(color("6")+"Page "+color("c")+"3 "+color("6")+"of "+color("c")+"4")
         return True
+    
     if args[0] == "4":
         sender.sendMessage("16 - "+color("1")+"Night Vision")
         sender.sendMessage("17 - "+color("a")+"Hunger")
@@ -73,6 +81,7 @@ def onCommandE(sender,args):
         sender.sendMessage("20 - "+color("8")+"Wither")
         sender.sendMessage(color("6")+"Page "+color("c")+"4 "+color("6")+"of "+color("c")+"4")
         return True
+    
     sender.sendMessage("01 - "+color("b")+"Speed")
     sender.sendMessage("02 - "+color("9")+"Slowness")
     sender.sendMessage("03 - "+color("e")+"Haste")
@@ -84,57 +93,80 @@ def onCommandE(sender,args):
 # Show chat colors
 @hook.command("c", description="Display each color with its respective character.")
 def onCommandC(sender, args):
-    sender.sendMessage(''.join([color("f"), "f ", color("7"), "7 ", color("e"), "e ", color("c"), "c ", color("d"), "d ", color("9"), "9 ", color("b"), "b ", color("a"), "a"]))
-    sender.sendMessage(''.join([color("0"), "0 ", color("8"), "8 ", color("6"), "6 ", color("4"), "4 ", color("5"), "5 ", color("1"), "1 ", color("3"), "3 ", color("2"), "2"]))
+    
+    sender.sendMessage(''.join([color("a"), "a ", color("b"), "b ", color("c"), "c ", color("d"), "d ", color("e"), "e ", color("f"), "f ", color("l"), "l ", color("m"), "m ", color("n"), "n ", color("o"), "o "]))
+    sender.sendMessage(''.join([color("0"), "0 ", color("1"), "1 ", color("2"), "2 ", color("3"), "3 ", color("4"), "4 ", color("5"), "5 ", color("6"), "6 ", color("7"), "7 ", color("8"), "8 ", color("9"), "9 "]))
     
     return True
     
 # AFK
 @hook.command("afk", description="Don't you dare.")
 def onCommandAFK(sender, args):
+    
     sender.sendMessage("Please do not go AFK, it wastes my bandwidth")
     sender.sendMessage("Instead, please log off the server")
+    
     return True
 
 # Save all
 @hook.command("save", description="Saves the map.")
 def onCommandSave(sender, args):                         
+    
     sudo("save-all")
+    
     return True
 
 # Random number
 @hook.command("random", description="Produce a random number.")
 def onCommandRandom(sender,args):
+    
     if len(args) == 1 and args[0].isdigit() == True:
         sender.sendMessage(str(random.randint(0,int(args[0]))))
+        return True
+    
     if len(args) == 2 and args[0].isdigit() == True and args[1].isdigit() == True:
+        
         if args[0] > args[1]:
             sender.sendMessage(str(random.randint(int(args[1]),int(args[0]))))
             return True
+        
         sender.sendMessage(str(random.randint(int(args[0]),int(args[1]))))
+        return True
+    
+    if len(args) == 3:
+        sender.sendMessage(str(random.randint(args[2]*int(args[0]),args[2]*int(args[1]))/args[2]))
+    
     if len(args) == 0:
         sender.sendMessage(str(random.randint(0,10)))
-    if len(args) > 2:
-        sender.sendMessage(color("c")+"Use the syntax "+color("6")+"/random [a] [b]")
-        return False
-    return True
+        return True
+    
+    sender.sendMessage(color("c")+"Use the syntax "+color("6")+"/random [a] [b]")
+    
+    return False
+    
                            
 # Item Renaming
 @hook.command("itemname", description="Rename and recolour an item!")
 def onCommandItemname(sender,args):
+    
     if len(args) == 0:
         sender.sendMessage(color("c")+"You must have an argument -"+color("6"),"/itemname [name] [format1] [format2] etc.")
     addpos = 0
     namestring = list(args[0])
+    
     for x in range(1,len(args)):
         addpos = addpos + 1
         for i in range(0, len(args[x])):
+            
             if itemnamewhitelist.count((args[x])[i]) == 1:
                 namestring.insert(addpos,color((args[x])[i]))
                 addpos = addpos + 2
+            
             else:
                 sender.sendMessage(color("c")+"Sorry, the format",color("6")+(args[x])[i],color("c")+"is not availible.")
-    sender.setDisplayName(sender.getItemInHand(),str(namestring))
+    
+    sender.sendMessage(str(displayname))
+    
     return True
 
 
